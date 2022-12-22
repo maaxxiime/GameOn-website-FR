a6152ea0dc010d59ac280417a00553096381f600
Maxime <97169969+maaxxiime@users.noreply.github.com>
2022-12-22 15:37:16 +0100

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,29 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementById('close-btn');
+const content = document.getElementById('content');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
+  modalbg.classList.remove("closemodal")
   modalbg.style.display = "block";
+
+}
+
+//remove modal event
+closeBtn.addEventListener("click", closeModal);
+
+//function remove modal
+function closeModal() {
+  modalbg.classList.add("closemodal");
+
+  setTimeout(() => {
+    modalbg.style.display = "none";
+  },700)
 }
 
 
