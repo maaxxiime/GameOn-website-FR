5a77783615f04163969ea6269a7c4a322490d13a
Maxime <97169969+maaxxiime@users.noreply.github.com>
2022-12-23 14:30:30 +0100

    dataset / error visible true

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,18 +10,20 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeBtn = document.getElementById('close-btn');
-const content = document.getElementById('content');
+const closeBtn = document.getElementById("close-btn");
+const content = document.getElementById("content");
+const form = document.getElementById("reserve");
+const modalBody = document.getElementById("modal-body");
+
+const first = document.getElementById("first");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.classList.remove("closemodal")
+  modalbg.classList.remove("closemodal");
   modalbg.style.display = "block";
-
 }
 
 //remove modal event
@@ -33,7 +35,17 @@ function closeModal() {
 
   setTimeout(() => {
     modalbg.style.display = "none";
-  },700)
+  }, 700);
 }
 
+//fonction validation de formulaire
+form.addEventListener("submit", function validate(e) {
+  e.preventDefault();
+  const formData = document.querySelectorAll(".formData");
+  console.log(formData)
 
+  formData[0].dataset.errorVisible = true
+
+});
+
+//modalBody.innerHTML = "Merci ! Votre réservation a été reçue."
