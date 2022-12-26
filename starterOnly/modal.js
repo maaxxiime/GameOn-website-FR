a17091d64fccb42632ed6175deb94472e38e7c37
Maxime <97169969+maaxxiime@users.noreply.github.com>
2022-12-26 11:59:28 +0100

    Validation de chaque input

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,8 +14,22 @@ const closeBtn = document.getElementById("close-btn");
 const content = document.getElementById("content");
 const form = document.getElementById("reserve");
 const modalBody = document.getElementById("modal-body");
+const formData = document.querySelectorAll(".formData");
 
-const first = document.getElementById("first");
+//input
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birthDate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const locationUn = document.getElementById('location1');
+const locationDeux = document.getElementById('location2');
+const locationTrois = document.getElementById('location3');
+const locationQuatre = document.getElementById('location4');
+const locationCinq = document.getElementById('location5');
+const locationSix = document.getElementById('location6');
+const checkboxUn = document.getElementById('checkbox1');
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -41,10 +55,78 @@ function closeModal() {
 //fonction validation de formulaire
 form.addEventListener("submit", function validate(e) {
   e.preventDefault();
-  const formData = document.querySelectorAll(".formData");
-  console.log(formData)
 
-  formData[0].dataset.errorVisible = true
+  const firstLength = first.value.length;
+  const lastLength = last.value.length;
+  const emailValue = email.value;
+  const birthDateLength = birthDate.value.length;
+  const quantityValue = quantity.value;
+  const locationUnChecked = locationUn.checked;
+  const locationDeuxChecked = locationDeux.checked;
+  const locationTroisChecked = locationTrois.checked;
+  const locationQuatreChecked = locationQuatre.checked;
+  const locationCinqChecked = locationCinq.checked;
+  const locationSixChecked = locationSix.checked;
+  const checkboxUnChecked = checkboxUn.checked;
+
+
+  //verifie les conditions de chaque input
+  if(firstLength <= 1 ) {
+    formData[0].dataset.errorVisible = true
+  } else {
+    console.log('Prenom à deux')
+    formData[0].dataset.errorVisible = false
+  }
+
+  if(lastLength <= 1 ) {
+    formData[1].dataset.errorVisible = true
+  } else {
+    console.log('Nom à deux')
+    formData[1].dataset.errorVisible = false
+  }
+
+  if(emailValue.includes('@') && emailValue.includes('.') && emailValue.length >= 6 ) {
+    console.log('Email valide')
+    formData[2].dataset.errorVisible = false
+  } else {
+    formData[2].dataset.errorVisible = true
+  }
+
+  if(birthDateLength == 10 ) {
+    formData[3].dataset.errorVisible = false
+    console.log('Date valide')
+  } else {
+    formData[3].dataset.errorVisible = true
+  }
+
+  if(quantityValue >= 0 && quantityValue <= 99 && quantityValue != quantityValue.includes("a") ) {
+    formData[4].dataset.errorVisible = false
+    console.log('Quantité valide')
+  } else {
+    formData[4].dataset.errorVisible = true
+  }
+
+  if(locationUnChecked === true || locationDeuxChecked === true || locationTroisChecked === true || locationQuatreChecked === true ||locationCinqChecked === true || locationSixChecked === true) {
+    formData[5].dataset.errorVisible = false
+    console.log('Location valide')
+  } else {
+    formData[5].dataset.errorVisible = true
+  }
+
+  if(checkboxUnChecked === true ) {
+    formData[6].dataset.errorVisible = false
+    console.log('Conditions valide')
+  } else {
+    formData[6].dataset.errorVisible = true
+  }
+
+  // affiche le message de succé si toutes les conditions sont valide
+  // if (firstLength <= 1 && lastLength <= 1 && emailValue.includes('@') && emailValue.includes('.') && emailValue.length >= 6 &&
+  // birthDateLength == 10 &&) {
+
+  // } else {
+
+  // }
 
 });
 
