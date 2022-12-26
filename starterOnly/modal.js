1b02caac92c56ce95c64d82f8730948491710934
Maxime <97169969+maaxxiime@users.noreply.github.com>
2022-12-26 14:29:31 +0100

    form success validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,21 +15,22 @@ const content = document.getElementById("content");
 const form = document.getElementById("reserve");
 const modalBody = document.getElementById("modal-body");
 const formData = document.querySelectorAll(".formData");
+const modalSuccess = document.getElementById("modal-success");
+const btnFermer = document.getElementById("btn-fermer");
 
 //input
-const first = document.getElementById('first');
-const last = document.getElementById('last');
-const email = document.getElementById('email');
-const birthDate = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
-const locationUn = document.getElementById('location1');
-const locationDeux = document.getElementById('location2');
-const locationTrois = document.getElementById('location3');
-const locationQuatre = document.getElementById('location4');
-const locationCinq = document.getElementById('location5');
-const locationSix = document.getElementById('location6');
-const checkboxUn = document.getElementById('checkbox1');
-
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locationUn = document.getElementById("location1");
+const locationDeux = document.getElementById("location2");
+const locationTrois = document.getElementById("location3");
+const locationQuatre = document.getElementById("location4");
+const locationCinq = document.getElementById("location5");
+const locationSix = document.getElementById("location6");
+const checkboxUn = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -52,6 +53,7 @@ function closeModal() {
   }, 700);
 }
 
+
 //fonction validation de formulaire
 form.addEventListener("submit", function validate(e) {
   e.preventDefault();
@@ -69,65 +71,108 @@ form.addEventListener("submit", function validate(e) {
   const locationSixChecked = locationSix.checked;
   const checkboxUnChecked = checkboxUn.checked;
 
-
   //verifie les conditions de chaque input
-  if(firstLength <= 1 ) {
-    formData[0].dataset.errorVisible = true
+  if (firstLength <= 1) {
+    formData[0].dataset.errorVisible = true;
   } else {
-    console.log('Prenom à deux')
-    formData[0].dataset.errorVisible = false
+    console.log("Prenom à deux");
+    formData[0].dataset.errorVisible = false;
   }
 
-  if(lastLength <= 1 ) {
-    formData[1].dataset.errorVisible = true
+  if (lastLength <= 1) {
+    formData[1].dataset.errorVisible = true;
   } else {
-    console.log('Nom à deux')
-    formData[1].dataset.errorVisible = false
+    console.log("Nom à deux");
+    formData[1].dataset.errorVisible = false;
   }
 
-  if(emailValue.includes('@') && emailValue.includes('.') && emailValue.length >= 6 ) {
-    console.log('Email valide')
-    formData[2].dataset.errorVisible = false
+  if (
+    emailValue.includes("@") &&
+    emailValue.includes(".") &&
+    emailValue.length >= 6
+  ) {
+    console.log("Email valide");
+    formData[2].dataset.errorVisible = false;
   } else {
-    formData[2].dataset.errorVisible = true
+    formData[2].dataset.errorVisible = true;
   }
 
-  if(birthDateLength == 10 ) {
-    formData[3].dataset.errorVisible = false
-    console.log('Date valide')
+  if (birthDateLength == 10) {
+    formData[3].dataset.errorVisible = false;
+    console.log("Date valide");
   } else {
-    formData[3].dataset.errorVisible = true
+    formData[3].dataset.errorVisible = true;
   }
 
-  if(quantityValue >= 0 && quantityValue <= 99 && quantityValue != quantityValue.includes("a") ) {
-    formData[4].dataset.errorVisible = false
-    console.log('Quantité valide')
+  if (
+    quantityValue >= 0 &&
+    quantityValue <= 99 &&
+    quantityValue != quantityValue.includes("a")
+  ) {
+    formData[4].dataset.errorVisible = false;
+    console.log("Quantité valide");
   } else {
-    formData[4].dataset.errorVisible = true
+    formData[4].dataset.errorVisible = true;
   }
 
-  if(locationUnChecked === true || locationDeuxChecked === true || locationTroisChecked === true || locationQuatreChecked === true ||locationCinqChecked === true || locationSixChecked === true) {
-    formData[5].dataset.errorVisible = false
-    console.log('Location valide')
+  if (
+    locationUnChecked === true ||
+    locationDeuxChecked === true ||
+    locationTroisChecked === true ||
+    locationQuatreChecked === true ||
+    locationCinqChecked === true ||
+    locationSixChecked === true
+  ) {
+    formData[5].dataset.errorVisible = false;
+    console.log("Location valide");
   } else {
-    formData[5].dataset.errorVisible = true
+    formData[5].dataset.errorVisible = true;
   }
 
-  if(checkboxUnChecked === true ) {
-    formData[6].dataset.errorVisible = false
-    console.log('Conditions valide')
+  if (checkboxUnChecked === true) {
+    formData[6].dataset.errorVisible = false;
+    console.log("Conditions valide");
   } else {
-    formData[6].dataset.errorVisible = true
+    formData[6].dataset.errorVisible = true;
   }
 
   // affiche le message de succé si toutes les conditions sont valide
-  // if (firstLength <= 1 && lastLength <= 1 && emailValue.includes('@') && emailValue.includes('.') && emailValue.length >= 6 &&
-  // birthDateLength == 10 &&) {
+  if (
+    (firstLength >= 2 &&
+      lastLength >= 2 &&
+      emailValue.includes("@") &&
+      emailValue.includes(".") &&
+      emailValue.length >= 6 &&
+      birthDateLength == 10 &&
+      quantityValue >= 0 &&
+      quantityValue <= 99 &&
+      quantityValue != quantityValue.includes("a") &&
+      locationUnChecked === true) ||
+    locationDeuxChecked === true ||
+    locationTroisChecked === true ||
+    locationQuatreChecked === true ||
+    locationCinqChecked === true ||
+    (locationSixChecked === true && checkboxUnChecked === true)
+  ) {
+    console.log("Tous les inputs sont corrects");
 
-  // } else {
+    modalBody.style.display = "none";
+    modalSuccess.style.display = "flex";
 
-  // }
-
+    setTimeout(() => {
+      modalBody.style.display = "block";
+      modalSuccess.style.display = "none";
+    }, 5000);
+  } else {
+    console.log("Erreur dans un ou plusieurs inputs");
+  }
 });
 
-//modalBody.innerHTML = "Merci ! Votre réservation a été reçue."
+
+// fermer modal event
+btnFermer.addEventListener("click", fermerModal);
+
+//function fermer modal
+function fermerModal() {
+  modalBody.style.display = "none";
+}
